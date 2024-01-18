ee116c8c87575d933daeab7de1b03635c24466dd
narrow.end.run <narendran.srinivasan@arista.com>
2024-01-18 12:24:21 +0530

    update viteconfig

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,23 @@
-import { build, defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { resolve } from 'path' 
+import { build, defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
-const root = resolve(__dirname,'src')
-const outDir = resolve(__dirname,'dist')
+const root = resolve(__dirname, "src");
+const outDir = resolve(__dirname, "dist");
 
 // https://vitejs.dev/config/
 export default defineConfig({
   root,
   plugins: [react()],
-  base:"/commagenetcp/",
-  build:{
+  base: "/commageneTCP/",
+  build: {
     outDir,
-    emptyOutDir:true,
-    rollupOptions:{
-      input:{
-        main : resolve(root,'index.html'),
-        rfepage : resolve(root,'rfe.html')
-      }
-    }
-  }
-
-})
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, "index.html"),
+        rfepage: resolve(root, "rfe.html"),
+      },
+    },
+  },
+});
