0b57bd08c4a6f657a205ed8bf0d8cb648fd09110
narrowendrun <82693670+narrowendrun@users.noreply.github.com>
2024-01-18 12:06:20 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ const outDir = resolve(__dirname,'dist')
 export default defineConfig({
   root,
   plugins: [react()],
-  base:"/tcpdump/",
+  base:"/commagenetcp/",
   build:{
     outDir,
     emptyOutDir:true,
